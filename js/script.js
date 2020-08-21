d1a070732a477b649057118c514e2eb209a0bdde
Eric Schorling <eric.schorling@yahoo.com>
2020-08-21 15:53:08 -0400

    updated with all fixes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
 'use strict';
 
-const input = document.querySelector('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
+const input = document.querySelector('.input'), // input/output button
+  numbers = document.querySelectorAll('.numbers div'), // number buttons
   operators = document.querySelectorAll('.operators div'), // operator buttons
   result = document.querySelector('#result'), // equal button
   clear = document.querySelector('#clear'); // clear button
 
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 let numberOperatorsArray = [];
+let operatorArray =['/','*','+','-']
+let equalPressed = false;
 
 numbers.forEach(function (number) {
   number.addEventListener('click', function () {
-    input.innerHTML += this.innerHTML;
-    numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    if(equalPressed) {
+        if(operatorArray.includes(numberOperatorsArray[numberOperatorsArray.length-1])){
+        input.innerHTML += this.innerHTML;
+        numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+        equalPressed = false
+    }}
+    else{
+      input.innerHTML += this.innerHTML;
+      numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    }
   });
 });
 
-operators.forEach(function (operater) {
+operators.forEach(function (operator) {
   operator.addEventListener('click', function () {
-    input.innerHTML += this.innerHTML;
-    numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    if (!operatorArray.includes(numberOperatorsArray[numberOperatorsArray.length-1])){
+      input.innerHTML += this.innerHTML;
+      numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    }
   });
 });
 
 // on click of 'equal' button
-result.addeventlistener('click', function () {
+result.addEventListener('click', function () {
   // do maths here...
-  let numbersStringHold = '';
+  let numbersStringHolder = '';
   let resultArray = [];
 
   for (let char of numberOperatorsArray) {
@@ -53,18 +65,19 @@ result.addeventlistener('click', function () {
 
   let add = resultArray.indexOf('+');
   while (add !== -1) {
-    resultArray.splice(add - 1, 3, resultArray[add - 1] - resultArray[add + 1]);
+    resultArray.splice(add - 1, 3, resultArray[add - 1] + resultArray[add + 1]);
     add = resultArray.indexOf('+');
   }
 
-  let subtract = resultArray.indexOf('+');
+  let subtract = resultArray.indexOf('-');
   while (subtract !== -1) {
-    resultArray.splice(subtract - 1,3,resultArray[subtract - 1] - resultArray[subtract - 1]);
+    resultArray.splice(subtract - 1,3,resultArray[subtract - 1] - resultArray[subtract + 1]);
     subtract = resultArray.indexOf('-');
   }
 
   numberOperatorsArray = [...resultArray];
-  input.innerHTML = 'numberOperatorsArray[0];';
+  input.innerHTML = numberOperatorsArray[0];
+  equalPressed = true
 
   console.log('Numbers and operators', numberOperatorsArray);
 });
@@ -72,5 +85,6 @@ result.addeventlistener('click', function () {
 // clearing the input on press of clear
 clear.addEventListener('click', function () {
   numberOperatorsArray = [];
-  input.innerHTML = 'CLEAR';
+  input.innerHTML = '';
+  equalPressed = false;
 });
